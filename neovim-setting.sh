11124b38f3b9ccc6c9129290c1953cd500502982
yutaroyamanaka <falcon4842@gmail.com>
2021-03-26 11:38:17 +0900

    alias is written in bashrc

--- neovim-setting.sh
+++ neovim-setting.sh
@@ -9,4 +9,4 @@ echo 'export PYENV_PYTHON3_VERSION=3.7.0' >> ~/.bash_profile
 # neovim install
 pip3 install neovim
 # set neovim default
-echo 'alias vim=nvim' >> ~/.bash_profile
+echo 'alias vim=nvim' >> ~/.bashrc
